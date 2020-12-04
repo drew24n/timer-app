04893939a689153066a9a85d83f4c3747d860bc4
Admin <drew24n@gmail.com>
2020-12-04 03:34:07 +0200

    logic refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -3,39 +3,49 @@ import styles from './App.module.scss';
 import {useDispatch, useSelector} from "react-redux";
 import {addTimer, removeTimer, setIntervalId, startStopTimer, updateTimerValue} from "./redux";
 import moment from "moment";
+import "moment-duration-format";
 
 export default function App() {
     const dispatch = useDispatch()
     const state = useSelector(state => state)
 
-    const [name, setName] = useState('')
+    const [timerName, setTimerName] = useState('')
 
     const addTimerHandler = (e) => {
         e.preventDefault()
+        const timerId = moment.now()
         dispatch(addTimer({
-            id: moment.now(),
+            id: timerId,
             intervalId: null,
-            name: name,
+            name: timerName || moment().format("YYYY-MM-DD"),
             value: 0,
             isRunning: false
         }))
-        setName('')
+        setTimerName('')
+        startTimer(timerId)
     }
 
     const deleteTimerHandler = (id) => {
         dispatch(removeTimer(id))
     }
 
-    const switchTimer = (id) => {
-        const currentTimer = state.timers.find(timer => timer.id === id)
+    const startTimer = (id) => {
+        dispatch(startStopTimer(id, true))
+        const intervalId = setInterval(() => dispatch(updateTimerValue(id, 1000)), 1000)
+        dispatch(setIntervalId(id, intervalId))
+    }
 
+    const stopTimer = (id, intervalId) => {
+        dispatch(startStopTimer(id, false))
+        clearInterval(intervalId)
+    }
+
+    const switchStartStop = (id) => {
+        const currentTimer = state.timers.find(timer => timer.id === id)
         if (!currentTimer.isRunning) {
-            dispatch(startStopTimer(id, true))
-            const intervalId = setInterval(() => dispatch(updateTimerValue(id, 1000)), 1000)
-            dispatch(setIntervalId(id, intervalId))
+            startTimer(id)
         } else {
-            dispatch(startStopTimer(id, false))
-            clearInterval(currentTimer.intervalId)
+            stopTimer(id, currentTimer.intervalId)
         }
     }
 
@@ -43,20 +53,19 @@ export default function App() {
         <div className={styles.container}>
             <h1>Tracker</h1>
             <form onSubmit={addTimerHandler}>
-                <input value={name} onChange={e => setName(e.target.value)} placeholder={'Enter tracker name'}
-                       type="text" required={true} autoFocus={true}/>
+                <input value={timerName} onChange={e => setTimerName(e.target.value)} placeholder={'Enter tracker name'}
+                       type="text" autoFocus/>
                 <button>add</button>
             </form>
             {state.timers.map(timer => {
                 return (
-                    <span key={timer.id}>
-                        <p>{timer.name} {moment.utc(timer.value).format('HH:mm:ss')}</p>
-                        <button
-                            onClick={() => switchTimer(timer.id)}>{timer.isRunning ? 'stop' : 'run'}</button>
+                    <div key={timer.id}>
+                        <p>{timer.name} {moment.duration(timer.value, 'ms').format('HH:mm:ss', {trim: false})}</p>
+                        <button onClick={() => switchStartStop(timer.id)}>{timer.isRunning ? 'stop' : 'run'}</button>
                         <button onClick={() => deleteTimerHandler(timer.id)}>del</button>
-                    </span>
+                    </div>
                 )
-            })}
+            }).reverse()}
         </div>
     )
 }
